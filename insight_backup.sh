912a61714f348bab345ff20164b4c6c28997aa36
Teremshonok Anton <TerAnYu@yandex.com>
2020-07-18 00:28:11 +0700

    Change api and fix bug.

--- insight_backup.sh
+++ insight_backup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# version: 20190617
+# version: 20200718
 # Writer: TerAnYu
 # need: command-line JSON processor - https://stedolan.github.io/jq/
 # wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq && chmod +x jq
 # curl with libssl
 
 BASEDIR=`dirname $0`
-url=http://127.0.0.1:8080
+url=http://localhost:8080
 listreq=/rest/insight/1.0/objectschema/list
-exportreq=/rest/insight/1.0/objectschema/export/server
+exportreq=/rest/insight/1.0/objectschemaexport/export/server
 username="localuser"
 password="localpassword"
 archpwd=123456
@@ -24,11 +24,12 @@ data=`curl -s \
     ids=`echo ${data} | "${BASEDIR}/jq" -r '.objectschemas[].id'`
     cnt=0
 
-for i in $ids; do
+for i in ${ids}; do
     name=`echo ${data} | "${BASEDIR}/jq" -r ".objectschemas[${cnt}].name"`
     countobj=`echo ${data} | "${BASEDIR}/jq" -r ".objectschemas[${cnt}].objectCount"`
 # echo output: "${cnt}; ${date}; ${i}; ${name}; ${countobj}"
    cnt=$((cnt+1))
+   sleep 5
 
 
 param(){
@@ -45,7 +46,7 @@ EOF
 }
 
 status_code=$(
-curl -s -u ${username}:${password} \
+curl -s -u "${username}":"${password}" \
         -H "Content-Type: application/json" \
         --write-out %{http_code} \
         --silent \
@@ -56,10 +57,10 @@ curl -s -u ${username}:${password} \
         "${url}${exportreq}"
 )
 
-if [ $status_code -ne 200 ] ; then
-    echo "Site bad status (${date}_${name}.zip): $status_code"
+if [ ${status_code} -ne 200 ] ; then
+    echo "Site bad status (${date}_${name}.zip): ${status_code}"
 else
-    echo "Site good status (${date}_${name}.zip): $status_code"
+    echo "Site good status (${date}_${name}.zip): ${status_code}"
 fi
 
 done
